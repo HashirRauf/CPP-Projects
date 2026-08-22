0981407dc609096e01eb151b9ec57598d326e0e9
Hashir Rauf <hashir.rauf.eng@gmail.com>
2026-08-22 17:50:29 +0500

    add loop to run this code again

--- Student Grade Calculator/student-grade-calculator.cpp
+++ Student Grade Calculator/student-grade-calculator.cpp
@@ -2,37 +2,44 @@
 using namespace std;
 int main()
 {
-    const int subj = 3;
-    string name;
-    int marks[3];
-    float total = 0, average = 0;
-    cout << "Enter your name: " << endl;
-    getline(cin, name);
-    for (int i = 0; i < subj; i++)
+    char choice;
+    do
     {
-        cout << "Enter marks for Subject: " << i << endl;
-        cin >> marks[i];
-    }
-    for (int i = 0; i < subj; i++)
-    {
-        total = total + marks[i];
-    }
-    average = total / subj;
-    cout << "Hi " << name << endl;
-    cout << "Total Marks: " << total << endl;
-    cout << "Average Marks is: " << average << endl;
-    if (average >= 70)
-    {
-        cout << "You got A grade" << endl;
-    }
-    else if (average >= 50 && average < 70)
-    {
-        cout << "You got B grade" << endl;
-    }
-    else if (average < 50)
-    {
-        cout << "You got C grade" << endl;
-    }
+        const int subj = 3;
+        string name;
+        int marks[3];
+        float total = 0, average = 0;
+        cout << "Enter your name: " << endl;
+        getline(cin, name);
+        for (int i = 0; i < subj; i++)
+        {
+            cout << "Enter marks for Subject: " << i << endl;
+            cin >> marks[i];
+        }
+        for (int i = 0; i < subj; i++)
+        {
+            total = total + marks[i];
+        }
+        average = total / subj;
+        cout << "Hi " << name << endl;
+        cout << "Total Marks: " << total << endl;
+        cout << "Average Marks is: " << average << endl;
+        if (average >= 70)
+        {
+            cout << "You got A grade" << endl;
+        }
+        else if (average >= 50 && average < 70)
+        {
+            cout << "You got B grade" << endl;
+        }
+        else if (average < 50)
+        {
+            cout << "You got C grade" << endl;
+        }
+        cout << "Do you want to Calculate Again(y/n): " << endl;
+        cin>>choice;
+
+    } while (choice == 'y' || 'Y');
 
     return 0;
 }
