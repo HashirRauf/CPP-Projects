de82445f1a6790159ea83e9d330d32105c10f50e
Hashir Rauf <hashir.rauf.eng@gmail.com>
2026-08-22 17:41:32 +0500

    Add student grade calculator with A/B/C grading

---
+++ Student Grade Calculator/student-grade-calculator.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int subj = 3;
+    string name;
+    int marks[3];
+    float total = 0, average = 0;
+    cout << "Enter your name: " << endl;
+    getline(cin, name);
+    for (int i = 0; i < subj; i++)
+    {
+        cout << "Enter marks for Subject: " << i << endl;
+        cin >> marks[i];
+    }
+    for (int i = 0; i < subj; i++)
+    {
+        total = total + marks[i];
+    }
+    average = total / subj;
+    cout << "Hi " << name << endl;
+    cout << "Total Marks: " << total << endl;
+    cout << "Average Marks is: " << average << endl;
+    if (average >= 70)
+    {
+        cout << "You got A grade" << endl;
+    }
+    else if (average >= 50 && average < 70)
+    {
+        cout << "You got B grade" << endl;
+    }
+    else if (average < 50)
+    {
+        cout << "You got C grade" << endl;
+    }
+
+    return 0;
+}
